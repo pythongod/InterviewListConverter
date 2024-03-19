1ad1bd70989b5252053a162797a8dfa44a203e82
pythongod <jankemper99@googlemail.com>
2024-03-19 14:01:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -259,7 +259,8 @@ function toggleSection(sectionId) {
 }
 
 function capitalize(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+//    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+    return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
 
